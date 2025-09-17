4ec3e38c31572b465e6762b4f32ec4ddd0b29291
Gabriel Vila Maior Teixeira <gabriel.vteixeira@al.infnet.edu.br>
2025-09-17 18:44:55 -0300

    adiciona fonte e corrige erro nos requisitos de tamanho

--- Assets/Scripts/Levels/Requirements/Size/MaximumSizeRequirement.cs
+++ Assets/Scripts/Levels/Requirements/Size/MaximumSizeRequirement.cs
@@ -8,9 +8,10 @@ public class MaximumSizeRequirement : SizeRequirement
 
     public override string GetText() => "Ter um tamanho menor que o indicado";
 
+    private readonly Vector3 _sizeLeeway = new Vector3(0.005f, 0.005f, 0.005f);
+
     protected override bool SizeCondition(Bounds sizeBounds, Bounds buildBounds)
     {
-        Debug.Log(buildBounds);
-        return sizeBounds.Contains(buildBounds.max) && sizeBounds.Contains(buildBounds.min);
+        return sizeBounds.Contains(buildBounds.max - _sizeLeeway) && sizeBounds.Contains(buildBounds.min + _sizeLeeway);
     }
 }
--- Assets/Scripts/Levels/Requirements/Size/MinimumSizeRequirement.cs
+++ Assets/Scripts/Levels/Requirements/Size/MinimumSizeRequirement.cs
@@ -8,8 +8,9 @@ public class MinimumSizeRequirement : SizeRequirement
 
     public override string GetText() => "Ter um tamanho maior que o indicado";
 
+    private readonly Vector3 _sizeLeeway = new Vector3(0.005f, 0.005f, 0.005f);
     protected override bool SizeCondition(Bounds sizeBounds, Bounds buildBounds)
     {
-        return buildBounds.Contains(sizeBounds.max) && buildBounds.Contains(sizeBounds.min);
+        return buildBounds.Contains(sizeBounds.max - _sizeLeeway) && buildBounds.Contains(sizeBounds.min + _sizeLeeway);
     }
 }
