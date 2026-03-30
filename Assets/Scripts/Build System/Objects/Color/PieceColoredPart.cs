769048c9380fd6175d05ce2c7fc3fd824e6be3e0
txera <gabrieltxera@gmail.com>
2026-03-30 12:41:18 -0300

    corrige erro na transparencia de peça

--- Assets/Scripts/Build System/Objects/Color/PieceColoredPart.cs
+++ Assets/Scripts/Build System/Objects/Color/PieceColoredPart.cs
@@ -1,5 +1,6 @@
 using System;
 using Reflex.Attributes;
+using Reflex.Extensions;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -11,14 +12,16 @@ public class PieceColoredPart : MonoBehaviour
     private bool _transparent;
     
     private Renderer _renderer;
+    private MaterialPropertyBlock _materialPropertyBlock;
     private static readonly int BaseColorPropertyId = Shader.PropertyToID("_BaseColor");
 
     [Inject]
-    private readonly PieceMaterials _pieceMaterials;
+    private PieceMaterials _pieceMaterials;
     
     private void Awake()
     {
         _renderer = GetComponent<Renderer>();
+        _materialPropertyBlock = new MaterialPropertyBlock();
 
         if (_coloredMaterialsIndexes == null || _coloredMaterialsIndexes.Length == 0)
             _coloredMaterialsIndexes = new[] { 0 };
@@ -26,17 +29,44 @@ public class PieceColoredPart : MonoBehaviour
 
     public void SetColor(Color color, bool transparent)
     {
+        if (_renderer == null)
+            return;
+
         if (transparent != _transparent)
         {
+            if (!TryResolvePieceMaterials(out var pieceMaterials))
+                return;
+
             _transparent = transparent;
-            _renderer.sharedMaterial = _pieceMaterials.GetMaterial(_transparent);
+            _renderer.sharedMaterial = pieceMaterials.GetMaterial(_transparent);
             _renderer.shadowCastingMode = _transparent ? ShadowCastingMode.Off : ShadowCastingMode.On;
         }
         
-        var materialPropertyBlock = new MaterialPropertyBlock();
-        materialPropertyBlock.SetColor(BaseColorPropertyId, color);
+        _materialPropertyBlock.Clear();
+        _materialPropertyBlock.SetColor(BaseColorPropertyId, color);
         
         foreach (var index in _coloredMaterialsIndexes)
-            _renderer.SetPropertyBlock(materialPropertyBlock, index);
+            _renderer.SetPropertyBlock(_materialPropertyBlock, index);
+    }
+
+    private bool TryResolvePieceMaterials(out PieceMaterials pieceMaterials)
+    {
+        if (_pieceMaterials != null)
+        {
+            pieceMaterials = _pieceMaterials;
+            return true;
+        }
+
+        try
+        {
+            _pieceMaterials = gameObject.scene.GetSceneContainer().Resolve<PieceMaterials>();
+        }
+        catch (Exception)
+        {
+            _pieceMaterials = null;
+        }
+
+        pieceMaterials = _pieceMaterials;
+        return pieceMaterials != null;
     }
 }
